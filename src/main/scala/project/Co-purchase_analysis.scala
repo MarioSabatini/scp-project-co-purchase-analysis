31c52f15effbd1a83e891b360388d9939b57a273
mariosabatini98 <mariosabatini98@gmaail.com>
2025-02-13 17:35:56 +0100

    output execution time

--- src/main/scala/project/Co-purchase_analysis.scala
+++ src/main/scala/project/Co-purchase_analysis.scala
@@ -1,51 +1,67 @@
 package example
 import org.apache.spark.sql.SparkSession
 import java.nio.file.{Files, Paths}
-import org.apache.spark.RangePartitioner
 import org.apache.spark.HashPartitioner
 import org.apache.spark.storage.StorageLevel
+import org.apache.spark.SparkConf
 
 object CoPurchaseAnalysis {
   def main(args: Array[String]): Unit = {
-
-    // Crea una sessione Spark
+  
+    // Sessione Spark
     val spark = SparkSession
       .builder()
       .appName("Co-Purchase Analysis")
       .getOrCreate()
-    // Ottieni il contesto Spark
     val sc = spark.sparkContext
+
+    // calcolo del numero di partizioni in base al numero di core e di nodi nel cluster
+    val numNodes = sc.getConf.get("spark.executor.instances")
+    val numCores = sc.getConf.get("spark.executor.cores")
+    val partitions = (numNodes.toInt * numCores.toInt * 3)
+   
 
     val outputDirectory = args(0) + "results"
 
-    // Leggi i dati da un file CSV
+    // Leggiamo i dati dal file CSV
     val filePath = args(0) + "order_products.csv"
+
+    // Tempo di esecuzione
+    val startTime = System.currentTimeMillis()
+
     val rdd = sc.textFile(filePath)
-
-    // Mappa per ottenere coppie di prodotti
+    .map(line => line.split(","))
+    .map(arr => (arr(0), arr(1)))
+    .partitionBy(new HashPartitioner(partitions))
+ 
+   
     val coPurchases = rdd
-      .map(line => line.split(","))
-      .map(arr => (arr(0), arr(1))) // (order_id, product_id)
-      .groupByKey() // Raggruppa per order_id
-      .flatMap { case (orderId, productIds) =>
-        val products = productIds.toArray.sorted // Ordina i prodotti
-        for {
-          i <- products.indices
-          j <- i + 1 until products.length
-        } yield {
-          // Poiché i prodotti sono già ordinati, non è necessario controllare l'ordine
-          ((products(i), products(j)), 1)
-        }
+    .groupByKey() // Raggruppiamo per order_id
+    .flatMap { case (orderId, productIds) =>
+      val products = productIds.toList 
+      for {
+        i <- products
+        j <- products if i < j
+      } yield {
+        ((i, j), 1)
       }
-
+    }
+    
     val coPurchasesPart =
-      coPurchases.partitionBy(new HashPartitioner(100))
-    // Riduci per sommare i conteggi delle coppie
+     coPurchases .partitionBy(new HashPartitioner(partitions))
+   
+    // Riduciamo per sommare i conteggi delle coppie
     val result = coPurchasesPart
-      .reduceByKey(_ + _) // Somma i conteggi delle coppie
+      .reduceByKey(_ + _)
       .map { case ((p1, p2), count) => (p1, p2, count) }
-
-    result.saveAsTextFile(outputDirectory)
+    // Riduciamo ad una sola partizione e salviamo il file
+    val singleResult = result.repartition(1)
+    singleResult.saveAsTextFile(outputDirectory)
+   
+    val endTime = System.currentTimeMillis()
+    val executionTime = endTime - startTime
+    // Stampiamo il tempo di esecuzione
+    println(s"Tempo di esecuzione: ${executionTime} ms")
     spark.stop()
   }
 }
